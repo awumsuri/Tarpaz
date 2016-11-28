53a354784d417f7230e1afc16f3acf06e247e8ec
Suri <awumsuri@gmail.com>
2016-11-28 00:24:17 -0500

    new

--- src/js/server.js
+++ src/js/server.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
+var fs = require("fs");
 
 mongoose.connect('mongodb://localhost:27017/TARPAZ', function(err) {
     if (err) throw err;
@@ -23,23 +24,55 @@ app.get('/', function(req, res) {
 });
 router.route("/update").post(function(req, res) {
 
-    var html = '<html><head><link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"\
-     rel="stylesheet"/><script src="https://code.jquery.com/jquery-3.1.1.min.js"\
-     integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="\
-    crossorigin="anonymous"></script><script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script></head>\
+    var html = '<html>\
+    <head>\
+    <link href="styles.css" rel="stylesheet"/>\
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet"/>\
+     <script src="https://code.jquery.com/jquery-3.1.1.min.js"\
+        integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="\
+        crossorigin="anonymous"></script><script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js">\
+    </script>\
+    </head>\
     <body>\
     <span class="space"></span>\
       <div class="logo">\
         <img src="tarpaz-logo.jpg"/>\
-      </div> <div class="main">\
+      </div>\
+      <div class="main">\
       <span class="title">GOLD PRICE AS OF<BR/>\
       <span id="time">\
-    <script>$("#time").html(new Date());</script></span></span>\
+    <script>$("#time").html(new Date());</script>\
+    </span></span>\
+    <div class ="prices"></div>\
+    <div class="form-group">\
+      <label class="col-md-4 control-label" for="textinput">FIX: $'+req.body.fix+'</label>\
+    </div>\
+    <div class="form-group">\
+      <label class="col-md-4 control-label" for="textinput">SILVER: $'+req.body.silver+'</label>\
+    </div>\
+    <div class="form-group">\
+      <label class="col-md-4 control-label" for="textinput">10K: $'+req.body.ten+'</label>\
+    </div>\
+    <div class="form-group">\
+      <label class="col-md-4 control-label" for="textinput">18K: $'+req.body.eighteen+'</label>\
+    </div>\
+    <div class="form-group">\
+      <label class="col-md-4 control-label" for="textinput">22K: $'+req.body.twentytwo+'</label>\
+    </div>\
+    <div class="form-group">\
+      <label class="col-md-4 control-label" for="textinput">PLT: $'+req.body.plt+'</label>\
+    </div>\
     </div>\
     </body>\
-    </html>'
+    </html>';
 
-    res.status('200').redirect("/static/");
+    fs.writeFile("public/index.html", html, function(err) {
+        if (err)
+            return console.log(err + ' ' + new Date());
+        console.log("new index saved successfully");
+
+        res.status('200').redirect("/static/");
+    });
 });
 
 router.route("/storetoken")
@@ -58,7 +91,7 @@ router.route("/storetoken")
                 return;
             }
 
-			console.log("token:"+req.body.token);
+            console.log("token:" + req.body.token);
 
             if (docs && docs.length > 0) {
                 res.send("success");
@@ -84,7 +117,7 @@ router.route("/storetoken")
 app.use("/api", router);
 app.use("/static", express.static("public"));
 //app.listen(port, '172.31.60.240', function(err) {
-  app.listen(port, '127.0.0.1', function(err) {
+app.listen(port, '127.0.0.1', function(err) {
     if (err) throw err;
     console.log("Conected to port:" + port);
 });
