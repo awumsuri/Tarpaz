b9cbf174b2d6500d582cdcf580b5984e1aad147f
Suri <awumsuri@gmail.com>
2016-11-30 14:53:54 -0500

    fixed urls

--- src/ios/TARPAZ DEMO/TARPAZ DEMO/TPConstants.swift
+++ src/ios/TARPAZ DEMO/TARPAZ DEMO/TPConstants.swift
@@ -10,7 +10,7 @@ import Foundation
 import UIKit
 
 open class TPConstants {
-    open static let DOMAIN:String = "http://104.236.85.29:8007";
+    open static let DOMAIN:String = "http://104.236.85.29";
     open static let SCREEN_WIDTH = UIScreen.main.bounds.size.width;
     open static let SCREEN_HEIGHT = UIScreen.main.bounds.size.height;
 }
